23ee913bd26d22638383e4e290a1c1787340ab41
herrizw <herrizw@gmail.com>
2020-09-24 03:52:39 -0700

    the index part is cloned using bootwatch

---
+++ app.js
@@ -0,0 +1,19 @@
+//Book class: Represents aBook
+
+class Book {
+    constructor(title, author, isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
+
+// UI Class: Handle UI Tasks
+
+// Store Class: Handles Storage
+
+// Event: Display Books
+
+// Event: Add a Book
+
+// Event: Remove a Book
